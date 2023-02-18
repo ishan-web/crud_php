cbb7b7f0a4da7f03240fb81da0ec2930da0c719a
Your Name <ishanpoudel58@gmail.com>
2023-02-18 21:26:38 +0545

    own

---
+++ trying.php
@@ -0,0 +1,23 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="try.css">
+</head>
+<body>
+    <!-- <h1>welcome to Vegas beiby</h1> -->
+    <div >
+        <nav class="navbar">
+            <h3>Navbar</h3>
+            <ul class="form">
+                <li ><a class="txt" href='profile'>Home</a></li>
+                <li ><a class="txt" href='profile'>Profile</li>
+                <li ><a class="txt" href='profile'>About us</li>
+                <li ><a class="txt" href='profile'>Contact</li>
+
+        </nav>
+    </div>
+
+</body>
+</html>
